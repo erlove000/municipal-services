dec4f7853741982380bb4a5a5079e6a3947db773
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2024-06-11 09:43:22 +0530

    rolling back sinle demand

--- ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationService.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.wscalculation.web.models.Calculation;
 import org.egov.wscalculation.web.models.CalculationReq;
-import org.egov.wscalculation.web.models.SingleDemand;
+// import org.egov.wscalculation.web.models.SingleDemand;
 public interface WSCalculationService {
 
 	List<Calculation> getCalculation(CalculationReq calculationReq);
@@ -13,6 +13,6 @@ public interface WSCalculationService {
 	void jobScheduler();
 
 	void generateDemandBasedOnTimePeriod(RequestInfo requestInfo);
-	void generateSingleDemand(SingleDemand singledemand);
+	// void generateSingleDemand(SingleDemand singledemand);
 	void generateBillBasedLocality(RequestInfo requestInfo);
 }
