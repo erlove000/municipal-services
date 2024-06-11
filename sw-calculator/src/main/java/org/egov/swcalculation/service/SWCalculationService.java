a9f2e2542b28222aa9bf89991ec0fe95a6992315
erlove000 <145665604+erlove000@users.noreply.github.com>
2024-06-11 15:27:40 +0530

    Changes for Single Demand

--- sw-calculator/src/main/java/org/egov/swcalculation/service/SWCalculationService.java
+++ sw-calculator/src/main/java/org/egov/swcalculation/service/SWCalculationService.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.swcalculation.web.models.Calculation;
 import org.egov.swcalculation.web.models.CalculationReq;
+import org.egov.swcalculation.web.models.SingleDemand;
+
 
 public interface SWCalculationService {
 	
 	List<Calculation> getCalculation(CalculationReq request);
 	
 	void generateDemandBasedOnTimePeriod(RequestInfo requestInfo);
+
+	void generateSingleDemand(SingleDemand singledemand);
 	
 	List<Calculation> getEstimation(CalculationReq request);
 	
